dc99acf60f6a18352cc3e777922482b2f32d7e03
himanshu <himanshu@debian.himanshu>
2025-09-24 14:04:57 +0530

    new: added the provider kubernetes block

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -91,8 +91,25 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region  = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  # version                = "~> 2.12"
+}
+
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.main.id
 }
 
 resource "aws_iam_role" "cluster" {
